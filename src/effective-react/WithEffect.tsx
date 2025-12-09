c6f5aef0115c7d940eb809fe2006afa644b8b3b1
Nathan Johnson <absurdprofit@icloud.com>
2025-12-09 09:34:30 -0500

    update WithEffect to manage state correctly

--- src/effective-react/WithEffect.tsx
+++ src/effective-react/WithEffect.tsx
@@ -1,5 +1,5 @@
 import { Effect, Layer, Ref } from "effect";
-import { use, useReducer, type JSX, type ReactNode } from "react";
+import { use, useReducer, useRef, type JSX, type ReactNode } from "react";
 import { ReactContext, REFS_SYMBOL, SCHEDULE_UPDATE_SYMBOL } from "./ReactContext";
 
 interface State {
@@ -16,40 +16,55 @@ export function WithEffect<P extends object>(
 	const store = new WeakMap<P, State>();
 	return function Component(props: P) {
 		const [, forceUpdate] = useReducer((c) => c + 1, Number());
+		const state = useRef(store.get(props) ?? { forceUpdate });
 		const propsChanged = !store.has(props);
-		const state = store.get(props) ?? { forceUpdate };
-		store.set(props, state);
+		store.set(props, state.current);
 		if (propsChanged) {
-			state.controller?.abort();
+			state.current.promise = undefined;
+			state.current.controller?.abort();
+			state.current.controller = undefined;
 		}
-		state.forceUpdate = () => {
-			state.promise = undefined;
-			state.controller?.abort();
-			state.controller = undefined;
+		state.current.forceUpdate = () => {
+			state.current.promise = undefined;
+			state.current.controller?.abort();
+			state.current.controller = undefined;
 			forceUpdate();
 		};
 
-		state.Refs ??= new Map();
-		state.controller ??= new AbortController();
-		const signal = state.controller.signal;
-		state.promise ??= Effect.runPromise(
+		state.current.Refs ??= new Map();
+		state.current.controller ??= new AbortController();
+		const signal = state.current.controller.signal;
+		state.current.promise ??= Effect.runPromise(
 			lambda(props)
 				.pipe(
 					Effect.provide(
 						Layer.succeed(
 							ReactContext,
 							{
-								[SCHEDULE_UPDATE_SYMBOL]: () => state.forceUpdate(),
-								[REFS_SYMBOL]: state.Refs
+								[SCHEDULE_UPDATE_SYMBOL]: () => state.current.forceUpdate(),
+								[REFS_SYMBOL]: state.current.Refs
 							}
 						)
 					)
 				),
 			{ signal }
-		);
-		const jsx = use(state.promise);
-		state.jsx = jsx;
+		).then(jsx => {
+			if (state.current.jsx) {
+				state.current.jsx = jsx;
+				forceUpdate();
+			}
 
-		return jsx;
+			return jsx;
+		});
+
+		if (state.current.jsx) {
+			return state.current.jsx;
+		} else {
+			const jsx = use(state.current.promise);
+			state.current.jsx = jsx;
+			
+			return jsx;
+		}
+
 	}
 }
