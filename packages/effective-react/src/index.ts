58f0422c48c619b606bcbe136e7a50c32d996e68
Nathan Johnson <absurdprofit@icloud.com>
2025-12-25 20:00:21 -0500

    Added exports

---
+++ packages/effective-react/src/index.ts
@@ -0,0 +1,6 @@
+export { CallbackEffect } from './CallbackEffect';
+export { ReactContext } from './ReactContext';
+export * as StateRef from './StateRef';
+export { UseState } from './UseState';
+export { UseRef } from './UseRef';
+export { WithEffect } from './WithEffect.tsx';
