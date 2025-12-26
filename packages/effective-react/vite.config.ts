d62c97fe62ac152946839b22518bfe88d64a1880
Nathan Johnson <absurdprofit@icloud.com>
2025-12-25 19:54:25 -0500

    Adding config files

---
+++ packages/effective-react/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({ outDir: 'build' }),
+  ],
+  test: {
+    globals: true,
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    browser: {
+      provider: 'playwright', // or 'webdriverio'
+      enabled: true,
+      name: 'chromium', // browser name is required
+      headless: process.argv.includes('--run'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+    },
+    target: 'ES2022',
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      output: {
+        entryFileNames: '[name].js',
+        dir: 'build',
+      },
+    },
+  },
+});
