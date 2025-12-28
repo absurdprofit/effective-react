727322888f5fa5b21ee8cebcead9a6bfd7e110e2
Nathan Johnson <absurdprofit@icloud.com>
2025-12-27 19:38:58 -0500

    Added UseDeferredValue hook

--- packages/effective-react/src/index.ts
+++ packages/effective-react/src/index.ts
@@ -2,5 +2,6 @@ export { CallbackEffect } from './CallbackEffect';
 export * as StateRef from './StateRef';
 export { UseState } from './UseState';
 export { UseRef } from './UseRef';
+export { UseDeferredValue } from './UseDeferredValue';
 export { WithEffect } from './WithEffect';
 export * from './Transition';
