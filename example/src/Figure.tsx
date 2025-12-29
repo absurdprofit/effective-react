0d39e260eabee6f4ddc0e49981ffd0e4dd09a180
Nathan Johnson <absurdprofit@icloud.com>
2025-12-29 11:25:49 -0500

    Added Figure current time update loop

--- example/src/Figure.tsx
+++ example/src/Figure.tsx
@@ -1,21 +1,24 @@
 import { ViewTransition } from 'react';
-import { Effect } from 'effect';
-import { EnableTransition, WithEffect } from '@absurdprofit/effective-react';
+import { Effect, Schedule } from 'effect';
+import { EnableTransition, StateRef, UseState, WithEffect } from '@absurdprofit/effective-react';
 import { ALL_STATUS_CODES } from './constants';
 
 const GLOBAL = {
   renders: Number(),
 };
 
+const SECOND_IN_MS = 1000;
+
 interface Props {
   index: number;
 }
 
+const useDate = new UseState();
 export const { Figure } = WithEffect((props: Props) =>
   Effect.gen(function* () {
     yield* EnableTransition;
     const statusCode = ALL_STATUS_CODES.at(props.index % ALL_STATUS_CODES.length);
-    const date = yield* Effect.sync(() => new Date());
+    const date = yield* useDate(Effect.sync(() => new Date().toString()));
     const dog = yield* Effect.tryPromise({
       try: async (signal) => {
         const response = await fetch(`/api/dog/${statusCode}.jpg`, { signal });
@@ -30,10 +33,17 @@ export const { Figure } = WithEffect((props: Props) =>
       Effect.catchAll(() => Effect.succeed({ url: '/api/dog/404.jpg' }))
     );
 
+    yield* Effect.forkScoped(
+      Effect.zip(
+        Effect.sleep(SECOND_IN_MS),
+        StateRef.set(date, new Date().toString())
+      )
+    );
+
     return (
       <div>
         <h1>Effect + React</h1>
-        <p>Current date and time: {date.toString()}</p>
+        <p>Current date and time: {yield* StateRef.get(date)}</p>
         <p>Index from parent: {props.index}</p>
         <p>Status Code: {statusCode}</p>
         <p>Renders so far: {++GLOBAL.renders}</p>
