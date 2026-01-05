b580524426d365a7fe72085ae2ade6303661e05d
Nathan Johnson <absurdprofit@icloud.com>
2026-01-04 19:37:34 -0500

    Added react compiler eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import reactCompiler from 'eslint-plugin-react-compiler'
+console.log(reactCompiler.configs.recommended)
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,6 +16,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactCompiler.configs.recommended
     ],
     languageOptions: {
       ecmaVersion: 2022,
