f915d3c90c9ef8dc0eb343c34346df79e6ac663e
Nathan Johnson <absurdprofit@icloud.com>
2026-01-07 16:50:19 -0500

    Removed playwright configs

--- packages/effective-react/vite.config.ts
+++ packages/effective-react/vite.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    browser: {
-      provider: playwright(),
-      enabled: true,
-      instances: [
-        { browser: 'chromium' },
-      ],
-      headless: process.argv.includes('--run'),
-    },
+    // browser: {
+    //   provider: playwright(),
+    //   enabled: true,
+    //   instances: [
+    //     { browser: 'chromium' },
+    //   ],
+    //   headless: process.argv.includes('--run'),
+    // },
   },
 });
