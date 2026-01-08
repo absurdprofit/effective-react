0376fcd80d807f2ee5b16eb81cad73de38d17082
Nathan Johnson <absurdprofit@icloud.com>
2026-01-08 10:07:54 -0500

    Fixed pipeline hanging behaviour

--- packages/effective-react-query/vite.config.ts
+++ packages/effective-react-query/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
+// import { playwright } from '@vitest/browser-playwright';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     globals: true,
     include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    browser: {
-      provider: playwright(),
-      enabled: true,
-      instances: [
-        { browser: 'chromium' },
-      ],
-      headless: process.argv.includes('--run'),
-    },
+    // browser: {
+    //   provider: playwright(),
+    //   enabled: true,
+    //   instances: [
+    //     { browser: 'chromium' },
+    //   ],
+    //   headless: process.argv.includes('--run'),
+    // },
   },
 });
