5d25b97986a48b2ad3492ed75b94e89376f242aa
Nathan Johnson <absurdprofit@icloud.com>
2026-01-08 20:16:50 -0500

    Refactor useTransitionEffect to accept Effect instead of lambda as input

--- packages/effective-react/src/startTransitionEffect.ts
+++ packages/effective-react/src/startTransitionEffect.ts
@@ -4,14 +4,14 @@ import { ReactContext } from './ReactContext';
 import { SET_TRANSITION_SYMBOL } from './common/constants';
 
 export function startTransitionEffect<R>(
-  lambda: () => Effect.Effect<void, never, R>
+  effect: Effect.Effect<void, never, R>
 ): Effect.Effect<void, never, R | ReactContext> {
   return Effect.gen(function* () {
     const context = yield* ReactContext;
     const runtime = yield* Effect.runtime<R>();
     startTransition(async () => {
       context[SET_TRANSITION_SYMBOL](true);
-      await Runtime.runPromise(runtime, lambda());
+      await Runtime.runPromise(runtime, effect);
     });
   });
 }
