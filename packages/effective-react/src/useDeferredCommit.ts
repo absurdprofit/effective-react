c84367fce07002c4e393ff4ed256590275df9675
Nathan Johnson <absurdprofit@icloud.com>
2026-01-10 20:55:11 -0500

    Refactor to avoid race condition use useDeferredCommit

--- packages/effective-react/src/useDeferredCommit.ts
+++ packages/effective-react/src/useDeferredCommit.ts
@@ -5,20 +5,21 @@ import { PHASE_SYMBOL } from './common/constants';
 export const useDeferredCommit = Effect.fn(function* () {
   const deferred = yield* Deferred.make<void>();
   const context = yield* ReactContext;
-  console.log(context[PHASE_SYMBOL].current);
-  if (context[PHASE_SYMBOL].current === 'committed') {
-    yield* Deferred.done(deferred, Exit.void);
-  } else {
-    const runtime = yield* Effect.runtime();
-    yield* Effect.sync(() => {
+  const runtime = yield* Effect.runtime();
+  yield* Effect.promise(async () => {
+    if (context[PHASE_SYMBOL].current === 'committed')
+      await Runtime.runPromise(
+        runtime,
+        Deferred.done(deferred, Exit.void)
+      );
+    else
       context[PHASE_SYMBOL].addEventListener('committed', async () => {
         await Runtime.runPromise(
           runtime,
           Deferred.done(deferred, Exit.void)
         );
       });
-    });
-  }
+  });
 
   return deferred;
 });
